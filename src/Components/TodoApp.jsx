d5cd128b76ac5fb199d2e13190856bbaf31a4eea
Tomas Koller <tomiskoller@gmail.com>
2024-07-16 23:00:53 +0200

    idk

--- src/Components/TodoApp.jsx
+++ src/Components/TodoApp.jsx
@@ -1,87 +1,100 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function TodoApp(){
+function TodoApp() {
+   const [todos, setTodos] = useState([]);
+   const [newTodo, setNewTodo] = useState("");
 
-    const [todos, setTodos] = useState([]);
-    const [newTodo, setNewTodo] = useState("");
+   function handleInputChange(event) {
+      setNewTodo(event.target.value);
+   }
 
-    function handleInputChange(event){
-        setNewTodo(event.target.value);
-    }
+   function addTodo() {
+      if (newTodo.trim() !== "") {
+         setTodos((t) => [...t, newTodo]);
+      }
+   }
 
-    function addTodo(){
-        if(newTodo.trim() !== ""){
-            setTodos(t => [...t, newTodo]);
-        }
-    }
+   function deleteTodo(index) {
+      const updatedTodos = todos.filter((_, i) => i !== index);
+      setTodos(updatedTodos);
+   }
 
-    function deleteTodo(index){
-        const updatedTodos = todos.filter((_, i) => i!==index);
-        setTodos(updatedTodos)
-    }
+   function moveTodoUp(index) {
+      if (index > 0) {
+         const updatedTodos = [...todos];
+         [updatedTodos[index], updatedTodos[index - 1]] = [
+            updatedTodos[index - 1],
+            updatedTodos[index],
+         ];
+         setTodos(updatedTodos);
+      }
+   }
 
-    function moveTodoUp(index){
-        if(index > 0){
-            const updatedTodos = [...todos];
-            [updatedTodos[index], updatedTodos[index-1]] =
-            [updatedTodos[index-1], updatedTodos[index]]
-            setTodos(updatedTodos);
-        }
-    }
+   function moveTodoDown(index) {
+      if (index < todos.length - 1) {
+         const updatedTodos = [...todos];
+         [updatedTodos[index], updatedTodos[index + 1]] = [
+            updatedTodos[index + 1],
+            updatedTodos[index],
+         ];
+         setTodos(updatedTodos);
+      }
+   }
 
-    function moveTodoDown(index){
-        if(index < todos.length - 1){
-            const updatedTodos = [...todos];
-            [updatedTodos[index], updatedTodos[index+1]] =
-            [updatedTodos[index+1], updatedTodos[index]]
-            setTodos(updatedTodos);
-        }
-    }
-
-    return<>
-        <div className="app-container">
+   return (
+      <>
+         <div className="app-container">
             <div className="header-container">
-                <h1>Todo App</h1>
+               <h1>Todo App</h1>
             </div>
 
             <div className="input-container">
-                <input value={newTodo} type="text" placeholder="Todo..." onChange={handleInputChange}/>
-                <button
-                onClick={addTodo}
-                >Add</button>
+               <input
+                  value={newTodo}
+                  type="text"
+                  placeholder="Todo..."
+                  onChange={handleInputChange}
+               />
+               <button onClick={addTodo}>Add</button>
             </div>
-            
+
             <div className="todos-container">
-                <ol>
-                    {todos.length < 1 ? <h1 className="info-msg"><i>Looks empty...</i></h1> :
-                    todos.map((todo, index) =>
+               <ol>
+                  {todos.length < 1 ? (
+                     <h1 className="info-msg">
+                        <i>Looks empty...</i>
+                     </h1>
+                  ) : (
+                     todos.map((todo, index) => (
                         <li key={index}>
-                            <span className="text">{todo}</span>
-                            <button 
-                            className="delete-button"
-                            onClick={() => deleteTodo(index)}
-                            >
-                                Delete
-                            </button>
+                           <span className="text">{todo}</span>
+                           <button
+                              className="delete-button"
+                              onClick={() => deleteTodo(index)}
+                           >
+                              Delete
+                           </button>
 
-                            <button 
-                            className="moveUp-button"
-                            onClick={() => moveTodoUp(index)}
-                            >Up
-                            </button>
-
-                            <button 
-                            className="moveDown-button"
-                            onClick={() => moveTodoDown(index)}
-                            >Down
-                            </button>
-                        </li>)
-                    }    
-                </ol> 
+                           <button
+                              className="moveUp-button"
+                              onClick={() => moveTodoUp(index)}
+                           >
+                              Up
+                           </button>
+                           <button
+                              className="moveDown-button"
+                              onClick={() => moveTodoDown(index)}
+                           >
+                              Down
+                           </button>
+                        </li>
+                     ))
+                  )}
+               </ol>
             </div>
-
-        </div>
-    </>
+         </div>
+      </>
+   );
 }
 
-export default TodoApp
+export default TodoApp;
